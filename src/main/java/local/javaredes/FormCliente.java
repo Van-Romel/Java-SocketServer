e88340e85004eea17ff44a03cecdc341ea0eebc2
Van-Romel A. S. Neto <vanr.neto@hotmail.com>
2024-02-24 23:05:56 +0000

    Atividade 03 completa.

---
+++ src/main/java/local/javaredes/FormCliente.java
@@ -0,0 +1,102 @@
+package local.javaredes;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Van-Romel Alves Sena Neto
+ */
+public class FormCliente implements ActionListener {
+
+    private static final FormCliente formCliente;
+    private static final JFrame janela;
+    private static final JLabel rotuloNome;
+    private static final JTextField caixaTextoNome;
+    private static final JLabel rotuloIdade;
+    private static final JTextField caixaTextoIdade;
+    private static final JLabel rotuloRetorno;
+    private static final JTextArea caixaTextoRetorno;
+    private static final JButton jButtonEnviar;
+
+    static {
+        formCliente = new FormCliente();
+        janela = new JFrame();
+        rotuloNome = new JLabel();
+        caixaTextoNome = new JTextField(55);
+        rotuloIdade = new JLabel();
+        caixaTextoIdade = new JTextField(55);
+        rotuloRetorno = new JLabel();
+        caixaTextoRetorno = new JTextArea(6, 55);
+        jButtonEnviar = new JButton();
+    }
+
+    private static Socket conexao;
+    private static DataInputStream entrada;
+    private static ObjectOutputStream saida;
+
+    public static void main(String[] args) {
+
+        janela.setSize(600, 300);
+        janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        janela.setLayout(new GridLayout(7,1));
+
+        rotuloNome.setText("Nome");
+        rotuloIdade.setText("Idade");
+        rotuloRetorno.setText("Retorno do Servidor");
+        caixaTextoRetorno.setEditable(false);
+        jButtonEnviar.setText("Enviar");
+        jButtonEnviar.addActionListener(formCliente);
+        jButtonEnviar.setFocusable(false);
+
+        janela.add(rotuloNome);
+        janela.add(caixaTextoNome);
+        janela.add(rotuloIdade);
+        janela.add(caixaTextoIdade);
+        janela.add(rotuloRetorno);
+        janela.add(caixaTextoRetorno);
+        janela.add(jButtonEnviar);
+        FlowLayout flowLayout = new FlowLayout();
+        flowLayout.setAlignment(FlowLayout.LEFT);
+        janela.setLayout(flowLayout);
+        janela.setLocationRelativeTo(null);
+        janela.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        if (evt.getSource().equals(jButtonEnviar)) {
+            Pessoa pessoa = new Pessoa();
+            pessoa.setNome(caixaTextoNome.getText());
+
+            pessoa.setIdade(Integer.parseInt(caixaTextoIdade.getText()));
+
+            try {
+                conexao = new Socket("127.0.0.1", 55000);
+                saida = new ObjectOutputStream(conexao.getOutputStream());
+
+                saida.writeObject(pessoa);
+
+                entrada = new DataInputStream(conexao.getInputStream());
+                String resposta = entrada.readUTF();
+                caixaTextoRetorno.setText(String.format("Recebeu do servidor: %n%s", resposta));
+
+                conexao.close();
+            } catch (IOException  exception) {
+                Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, exception);
+            }
+        }
+    }
+}
--- src/main/java/local/javaredes/Servidor.java
+++ src/main/java/local/javaredes/Servidor.java
@@ -1,8 +1,8 @@
 package local.javaredes;
 
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -17,78 +17,28 @@ public class Servidor {
 
     private static ServerSocket servidor;
     private static Socket conexao;
-    private static DataInputStream entrada;
+    private static ObjectInputStream entrada;
     private static DataOutputStream saida;
 
     public static void main(String[] args) {
 
         try {
             servidor = new ServerSocket(55000);
+            System.out.println("Aguardando conexao...");
             conexao = servidor.accept();
 
-            entrada = new DataInputStream(conexao.getInputStream());
-            String valor = entrada.readUTF();
-
-            // valida cpf
-            String resultado = "";
-            resultado = validaCpf(valor) ? "Este CPF é válido." : "Este CPF é inválido.";
+            entrada = new ObjectInputStream(conexao.getInputStream());
+            Pessoa pessoa = (Pessoa) entrada.readObject();
+            System.out.printf("Nome: %s%n", pessoa.getNome());
+            System.out.printf("Idade: %d", pessoa.getIdade());
 
             saida = new DataOutputStream(conexao.getOutputStream());
-            saida.writeUTF(resultado);
+
+            saida.writeUTF("Dados recebidos corretamente!");
 
             conexao.close();
-        } catch (IOException exception) {
+        } catch (IOException | ClassNotFoundException exception) {
             Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, exception);
         }
-    }
-
-    private static boolean validaCpf(String cpf) {
-
-        // Este algoritimo foi feito e implementado por mim com inspiração do recurso B do PDF da atividade.
-        // Recurso B = https://www.devmedia.com.br/validando-o-cpf-em-uma-aplicacao-java/22097
-
-        cpf = cpf.replaceAll("\\.", "")
-                .replaceAll("-", "");
-
-        if (cpf.equals("00000000000") ||
-                cpf.equals("11111111111") ||
-                cpf.equals("22222222222") || cpf.equals("33333333333") ||
-                cpf.equals("44444444444") || cpf.equals("55555555555") ||
-                cpf.equals("66666666666") || cpf.equals("77777777777") ||
-                cpf.equals("88888888888") || cpf.equals("99999999999") ||
-                (!cpf.matches("^(\\d{3}\\.){2}\\d{3}-\\d{2}|\\d{11}$")))
-            return(false);
-
-        int somaValoresDigitos = 0;
-        int auxiliarFatorialInversa = 10;
-        int numero;
-        char dig10, dig11;
-        for (int i = 0; i< 9; i++) {
-            numero = (cpf.charAt(i) - 48);
-            somaValoresDigitos = somaValoresDigitos + (numero * auxiliarFatorialInversa);
-            auxiliarFatorialInversa = auxiliarFatorialInversa - 1;
-        }
-
-        int resultado = 11 - (somaValoresDigitos % 11);
-        if ((resultado == 10) || (resultado == 11))
-            dig10 = '0';
-        else dig10 = (char)(resultado + 48);
-
-        somaValoresDigitos = 0;
-        auxiliarFatorialInversa = 11;
-        for(int i = 0; i < 10; i++) {
-            numero = (int)(cpf.charAt(i) - 48);
-            somaValoresDigitos = somaValoresDigitos + (numero * auxiliarFatorialInversa);
-            auxiliarFatorialInversa = auxiliarFatorialInversa - 1;
-        }
-
-        resultado = 11 - (somaValoresDigitos % 11);
-        if ((resultado == 10) || (resultado == 11))
-            dig11 = '0';
-        else dig11 = (char)(resultado + 48);
-
-        if ((dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10)))
-            return(true);
-        else return(false);
     }
 }
